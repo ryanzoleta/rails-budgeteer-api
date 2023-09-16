31c862e8fe356d8e756ff77cb58cee54aab5714e
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-09-16 23:47:19 +0800

    finish controllers

--- app/controllers/account_types_controller.rb
+++ app/controllers/account_types_controller.rb
@@ -3,4 +3,39 @@ class AccountTypesController < ApplicationController
     @account_types = AccountType.all
     render json: @account_types
   end
+
+  def create
+    @account_type = AccountType.new({ name: params[:name] })
+
+    if @account_type.save
+      render json: @account_type
+    else
+      render json: { errors: @account_type.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @account_type = AccountType.find(params[:id])
+
+    render json: @account_type
+  end
+
+  def update
+    @account_type = AccountType.find(params[:id])
+
+    @account_type.name = params[:name]
+
+    if @account_type.save
+      render json: @account_type
+    else
+      render json: { errors: @account_type.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @account_type = AccountType.find(params[:id])
+
+    @account_type.destroy
+    render json: 'record deleted', status: 200
+  end
 end
--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -13,4 +13,30 @@ class AccountsController < ApplicationController
       render json: { errors: @account.errors.full_messages }, status: :unprocessable_entity
     end
   end
+
+  def show
+    @account = Account.find(params[:id])
+
+    render json: @account
+  end
+
+  def update
+    @account = Account.find(params[:id])
+
+    @account.name = params[:name]
+    @account.account_type_id = params[:account_type_id]
+
+    if @account.save
+      render json: @account
+    else
+      render json: { errors: @account.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @account = Account.find(params[:id])
+
+    @account.destroy
+    render json: 'record deleted', status: 200
+  end
 end
